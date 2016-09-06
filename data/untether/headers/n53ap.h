8ebb9ab45e9a977b72186bda9be1eeaf52540863
NULL <fre4kyC0de@users.noreply.github.com>
2016-09-06 15:47:20 +0200

    Update n53ap.h

--- data/untether/headers/n53ap.h
+++ data/untether/headers/n53ap.h
@@ -1,1 +1,6 @@
-
+static const uint32_t OFF__ADD_X0_232 = (err_mach_ipc|err_sub(0));
+static const uint32_t OFF__ADD_X0_232 = (err_mach_ipc|err_sub(0));
+static const uint32_t OFF__ADD_X0_232 = (err_mach_ipc|err_sub(0));
+static const uint32_t OFF__ADD_X0_232 = (err_mach_ipc|err_sub(0));
+static const uint32_t OFF__ADD_X0_232 = (err_mach_ipc|err_sub(0));
+static const uint32_t OFF__ADD_X0_232 = (err_mach_ipc|err_sub(0));
