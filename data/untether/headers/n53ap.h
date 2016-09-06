3d2b9317260c06c0a5646d123619b584ddea24d0
NULL <fre4kyC0de@users.noreply.github.com>
2016-09-06 15:54:38 +0200

    move n53 offsets to it's own file

--- data/untether/headers/n53ap.h
+++ data/untether/headers/n53ap.h
@@ -1,6 +1,24 @@
-static const uint32_t OFF__ADD_X0_232 = (err_mach_ipc|err_sub(0));
-static const uint32_t OFF__ADD_X0_232 = (err_mach_ipc|err_sub(0));
-static const uint32_t OFF__ADD_X0_232 = (err_mach_ipc|err_sub(0));
-static const uint32_t OFF__ADD_X0_232 = (err_mach_ipc|err_sub(0));
-static const uint32_t OFF__ADD_X0_232 = (err_mach_ipc|err_sub(0));
-static const uint32_t OFF__ADD_X0_232 = (err_mach_ipc|err_sub(0));
+static const uint32_t OFF__ADD_X0_232             = 0x0F586C;
+static const uint32_t OFF__LDR_X0_X8_W1_SXTW_2    = 0x3834C8;
+static const uint32_t OFF__STRB_W1_X8_W2_UXTW     = 0x3A13CC;
+static const uint32_t OFF__LDR_X0_X1_32           = 0x3DA2AC;
+static const uint32_t OFF__STR_W3_X1_W2_UXTW      = 0xED4670;
+
+static const uint32_t OFF__INVALIDATE_TLB         = 0x0DEAD0;
+static const uint32_t OFF__FLUSHCACHE             = 0x0CDC84;
+
+static const uint32_t OFF__AMFI_GET_OUT_OF_MY_WAY = 0x6AF290;
+static const uint32_t OFF__MOUNT_COMMON           = 0x110ABC;
+static const uint32_t OFF__CS_ENFORCE             = 0x6AF291;
+static const uint32_t OFF__VM_MAP_ENTER           = 0x084648;
+static const uint32_t OFF__VM_MAP_PROTECT         = 0x086298;
+static const uint32_t OFF__TFP0                   = 0x37C2B0;
+static const uint32_t OFF__GET_R00T               = 0x314E98;
+static const uint32_t OFF__ICHDB_1                = 0x526804;
+static const uint32_t OFF__ICHDB_2                = 0x525EF4;
+static const uint32_t OFF__PROC_ENFORCE           = 0x4BEF64;
+static const uint32_t OFF__MAPIO                  = 0xFBDEEC;
+static const uint32_t OFF__SB_TRACE               = 0xCC1750;
+
+static const uint32_t OFF__KERNEL_PMAP            = 0x4AA138;
+static const uint32_t OFF__PHYS_ADDR              = 0x540040;
