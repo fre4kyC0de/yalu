a25e41cb1e341368d84050fd21e5dcf256e7f270
NULL <fre4kyC0de@users.noreply.github.com>
2016-09-06 15:45:10 +0200

    Update make.sh

--- data/untether/make.sh
+++ data/untether/make.sh
@@ -1,5 +1,2 @@
 gcc -std=c++14 untether64.mm -o amfistop64 -w -arch armv7 -isysroot "$(xcrun --show-sdk-path --sdk iphoneos)" -framework IOKit -framework Foundation -lz
 ldid -Se.xml amfistop64
-
-gcc -std=c++14 untether64.mm -o untether64 -w -arch arm64 -isysroot "$(xcrun --show-sdk-path --sdk iphoneos)" -framework IOKit -framework Foundation -lz
-ldid -Se.xml untether64
